f474fe68c2bab35a2344a8f24132611ca256f258
ishanjain28 <ishanjain28@gmail.com>
2016-07-06 21:41:05 +0530

    Redesigning it from scratch using browserify and in a more appropriate manner

--- README.md
+++
@@ -1,3 +0,0 @@
-# Random Quote Machine
-This is a Repository of my Random Quotes Project built using ReactJS
-All the necessary files are included. Just open index.html and it should work. 
---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+var React = require('react');
+var $ = require('jquery');
+
+const Navbar = React.createClass({
+	render: function () {
+		return (
+			<div className="navbar">
+			<p className="header">RANDOM QUOTES</p>
+		</div>
+		);
+	}
+});
+
+const QuoteWrapper = React.createClass({
+    getInitialState: function() {
+        return {
+            quote: 'Quote here',
+            author: 'Who said it?',
+            category: 'programming'
+        }
+    },
+    getCategory: function () {
+
+    },
+    render: function () {
+        return (
+            <div className="QuoteWrapper">
+                <QuoteBox 
+                    quote={this.state.quote} 
+                    author={this.state.author}
+                    getCategory={this.getCategory}
+                    category={this.state.category}
+                />
+            </div>
+        );
+    }
+});
+
+const QuoteBox = React.createClass({
+    render: function () {
+        $('.categoryLI li').on('click', function () {
+            var activeCategory = 'categoryLI ' + this.props.category + ' categoryLIActive';
+                $('li').addClass(activeCategory);
+        });
+        return (
+            <div className="QuoteBox">
+            <div className="quote">
+                {this.props.quote}
+            </div>
+            <div className="author">
+                <i>-{this.props.author}</i>
+            </div>
+            <div className="category" onClick={this.props.category}>
+            <i>
+                <ul className="categoryUL">
+                    <li className="categoryLI movies">#movies</li>
+                    <li className="categoryLI famous">#famous</li>
+                    <li className="categoryLI programming">#programming</li>
+                </ul>
+            </i>
+            </div>
+            <div className="ButtonBox">
+                <NewQuote />
+                <Tweet />
+            </div>
+            </div>
+        );
+    }
+});
+
+const NewQuote = React.createClass({
+    render: function () {
+        return (
+            <div className="newquote">
+                New Quote
+            </div>
+        );
+    }
+});
+
+const Tweet = React.createClass({
+    render: function () {
+        return (
+            <div className="tweet">
+                tweet
+            </div>
+        );
+    }
+});
+
+const App = React.createClass({
+    render: function () {
+        return (
+            <div className="app">
+                <Navbar />
+                <QuoteWrapper />
+            </div>
+        );
+    }
+})
+module.exports = App;
